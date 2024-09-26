38fd144ab83959f29d6ee1eafe99d9da2e1c4280
ПК <bic3337@gmail.com>
2024-09-26 18:53:20 +0300

    Выполнил домашнюю работу.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,59 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Home Work !");
+       var dog = 8.0;
+       System.out.println(dog);
+       var cat = 3.6;
+       System.out.println(cat);
+       var paper = 763789;
+       System.out.println(paper);
+dog = dog+4;
+System.out.println(dog);
+cat = cat+4;
+System.out.println(cat);
+paper = paper + 4;
+System.out.println(paper);
+dog = dog - 3.5;
+System.out.println(dog);
+cat = cat - 1.6;
+System.out.println(cat);
+paper = paper - 7639;
+System.out.println(paper);
+var friend = 19;
+System.out.println(friend);
+friend = friend + 2 ;
+System.out.println(friend);
+friend = friend /7;
+System.out.println(friend);
+var frog = 3.5;
+System.out.println(frog);
+frog = frog * 10;
+System.out.println(frog);
+frog = frog / 3.5;
+System.out.println(frog);
+frog = frog + 4;
+System.out.println(frog);
+var oneBoxer = 78.2;
+var twoBoxer = 82.7;
+var boxerWeigth = oneBoxer + twoBoxer  ;
+System.out.println("Обший вес " + boxerWeigth + " кг.");
+var advantage = twoBoxer-oneBoxer;
+System.out.println("разница в весе на " + advantage + "кг.");
+var underweight =(twoBoxer-oneBoxer)%boxerWeigth;
+System.out.println("перевес боксера над другим на " + underweight +  "кг.");
+var totalTime = 640;
+var workingTime = 8;
+var employees = totalTime / workingTime;
+System.out.println("Всего сотрудников вкомпани - " +employees +" человек.");
+var moreEmployees = 94;
+var workingHours = (moreEmployees+ employees )*workingTime;
+System.out.println("Если в компании работает "+ moreEmployees+ " челове,то всего "+ workingHours +" часов работы может быть поделено между сотрудниками. ");
+
+
+
+
+
 
     }
+
 }
